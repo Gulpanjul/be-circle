ca629c19b7862cf94394f34ad17cc7581267eb8f
Gulpanjul <gulpa.andhikac@gmail.com>
2025-06-26 05:44:09 +0700

    chore: swagger-version

--- src/docs/routes.ts
+++ src/docs/routes.ts
@@ -1,5 +1,6 @@
 import { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
+
 import swaggerOutput from './swagger-output.json';
 import fs from 'fs';
 import path from 'path';
@@ -16,6 +17,23 @@ export default function docs(app: Express) {
   app.use(
     '/api-docs',
     swaggerUi.serve,
-    swaggerUi.setup(swaggerOutput, { customCss: css }),
+    swaggerUi.setup(swaggerOutput, {
+      customCss:
+        css +
+        `
+        .swagger-ui .topbar { display: none; }
+        .information-container.wrapper { background:#04A51E; padding:2rem; }
+        .information-container .info .main .title,
+        .swagger-ui .renderedMarkdown p,
+        .swagger-ui .info .description,
+        .swagger-ui .info {
+        color: white !important;
+      }
+      `,
+      customSiteTitle: 'Circle App API',
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    }),
   );
 }
