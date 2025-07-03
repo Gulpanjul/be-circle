9eb018e6dab50516b439fcb09700ec4540efb6ea
Gulpanjul <gulpa.andhikac@gmail.com>
2025-07-03 20:58:16 +0700

    fix: allow empty string for avatarUrl in profile update validation schema

--- src/validations/profile.validation.ts
+++ src/validations/profile.validation.ts
@@ -5,5 +5,5 @@ export const updateProfileSchema = Joi.object<updatedUserProfileDTO>({
   fullName: Joi.string().min(4),
   username: Joi.string().min(4).max(12),
   bio: Joi.string().min(4).max(100),
-  avatarUrl: Joi.string().optional(),
+  avatarUrl: Joi.string().allow('').optional(),
 });
